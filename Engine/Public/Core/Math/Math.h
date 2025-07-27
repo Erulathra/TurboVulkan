9d284418fd6b0442b8ec298ccac6eeb850b0469c
Szymon Świędrych <43550433+S1NU5-P1@users.noreply.github.com>
2025-07-27 14:38:49 +0200

    Add `glm::vec<L,T,Q>` formater

--- Engine/Public/Core/Math/Math.h
+++ Engine/Public/Core/Math/Math.h
@@ -7,21 +7,38 @@
 #define TURBO_SMALL_NUMBER 1e-5
 #define TURBO_VERY_SMALL_NUMBER 1e-8
 
-namespace Turbo {
-
-class FMath {
-public:
-	template <typename T> requires (std::is_floating_point_v<T>)
-	static bool NearlyEqual(const T& a, const T& b, const T& epsilon = static_cast<T>(TURBO_VERY_SMALL_NUMBER))
+namespace Turbo
+{
+	class FMath
 	{
-		return glm::epsilonEqual(a, b, epsilon);
-	}
+	public:
+		template <typename T> requires (std::is_floating_point_v<T>)
+		static bool NearlyEqual(const T& a, const T& b, const T& epsilon = static_cast<T>(TURBO_VERY_SMALL_NUMBER))
+		{
+			return glm::epsilonEqual(a, b, epsilon);
+		}
 
-	template <typename T> requires (std::is_floating_point_v<T>)
-	static bool NearlyZero(const T& a, const T& epsilon = static_cast<T>(TURBO_VERY_SMALL_NUMBER))
+		template <typename T> requires (std::is_floating_point_v<T>)
+		static bool NearlyZero(const T& a, const T& epsilon = static_cast<T>(TURBO_VERY_SMALL_NUMBER))
+		{
+			return glm::epsilonEqual(a, static_cast<T>(0), epsilon);
+		}
+	};
+} // Turbo
+
+template <int32 L, typename T>
+struct fmt::formatter<glm::vec<L, T>> : fmt::formatter<std::string>
+{
+	auto format(const glm::vec<L, T>& vector, format_context& ctx) const
 	{
-		return glm::epsilonEqual(a, static_cast<T>(0), epsilon);
+		std::stringstream resultStream;
+		resultStream << '(';
+		for (int componentIndex = 0; componentIndex < vector.length() - 1; ++componentIndex)
+		{
+			resultStream << vector[componentIndex] << ", ";
+		}
+		resultStream << vector[vector.length() - 1] << ")";
+
+		return formatter<std::string>::format(resultStream.str(), ctx);
 	}
 };
-
-} // Turbo
